b01c89b8fa77dac964288e42f0ede71e0d8426d0
= <=>
2026-02-25 22:23:35 +0000

    starting Neural Net

---
+++ neural_net.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import scipy as sp
+import numpy as np
+import math
+
+def ReLU(x=int):
+    if x > 0:
+        return x
+    else:
+        return 0
+
+def forward(w = list, nodes = list):
+    next_node = np.dot()
+
+def Neural_Net(n_layers = int, n_nodes = list):
+    

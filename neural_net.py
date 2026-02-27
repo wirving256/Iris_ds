46fea6e89de4676889d44cd76378ecfa8b2ee8b0
= <=>
2026-02-27 11:44:43 +0000

    reorganised and added final node computation

--- neural_net.py
+++ neural_net.py
@@ -2,13 +2,27 @@ import pandas as pd
 import scipy as sp
 import numpy as np
 
+def Neural_Net(n_layers, n_nodes):
+    if not isinstance(n_layers, int):
+        raise TypeError("expected type integer")
+    if not isinstance(n_nodes, np.ndarray):
+        raise TypeError("expected type np.ndarray")
+    if n_layers != len(n_nodes):
+        raise IndexError(f"number of layers ({n_layers}) given by variable n_layers is different to the number of layers given ({len(n_nodes)}) by n_nodes")
+    return n_nodes
+
+
+
+
+
+
+
 def ReLU(x):
     if not isinstance(x, np.ndarray):
         raise TypeError("exxpected type np.ndarray")
     for i, index in enumerate(x):
         if x[i] < 0:
             x[i] = 0
-
     return x
 
 def forward(w, prev_nodes):
@@ -19,15 +33,13 @@ def forward(w, prev_nodes):
     next_node = np.dot(w,prev_nodes)
     return next_node
 
-def Neural_Net(n_layers, n_nodes):
-    if not isinstance(n_layers, int):
-        raise TypeError("expected type integer")
-    if not isinstance(n_nodes, np.ndarray):
+def final_layer(w, prev_nodes):
+    if not isinstance(w, np.ndarray):
         raise TypeError("expected type np.ndarray")
-    if n_layers != len(n_nodes):
-        raise IndexError(f"number of layers ({n_layers}) given by variable n_layers is different to the number of layers given ({len(n_nodes)}) by n_nodes")
-    return n_layers
-
+    if not isinstance(prev_nodes, np.ndarray):
+        raise TypeError("expected type np.ndarray")
+    last_node = np.dot(w,prev_nodes)
+    return last_node
 #n = Neural_Net(n_layers=2, n_nodes=np.array([2,3]))
 #print(n)
 
